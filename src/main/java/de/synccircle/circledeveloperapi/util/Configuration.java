2c121659e31fa5ca4ce50109de67cb3657a9f179
Bennit B. <bennit.br+github@gmail.com>
2025-06-29 23:17:42 +0200

    add: save method

--- src/main/java/de/synccircle/circledeveloperapi/util/Configuration.java
+++ src/main/java/de/synccircle/circledeveloperapi/util/Configuration.java
@@ -3,6 +3,15 @@ package de.synccircle.circledeveloperapi.util;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
+import java.io.IOException;
 
 public record Configuration(File file, YamlConfiguration configuration) {
+
+    public void save() {
+        try {
+            this.configuration.save(this.file);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
