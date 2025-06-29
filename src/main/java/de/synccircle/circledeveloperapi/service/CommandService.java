c9c1dbcdedf445ad6a8135f2f96cccb7599c091f
Bennit B. <bennit.br+github@gmail.com>
2025-06-29 23:17:30 +0200

    create: CommandService to register Commands

---
+++ src/main/java/de/synccircle/circledeveloperapi/service/CommandService.java
@@ -0,0 +1,38 @@
+package de.synccircle.circledeveloperapi.service;
+
+import de.synccircle.circledeveloperapi.CircleDeveloperAPI;
+import de.synccircle.circledeveloperapi.util.Configuration;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.plugin.Plugin;
+
+import java.io.IOException;
+
+public class CommandService {
+
+    private final CircleDeveloperAPI plugin;
+
+    public CommandService(CircleDeveloperAPI plugin) {
+        this.plugin = plugin;
+    }
+
+    public void registerCommand(Plugin commandPlugin, String name, CommandExecutor executor) {
+        PluginCommand command = commandPlugin.getServer().getPluginCommand(name);
+        if(command != null) {
+            Configuration configuration;
+            try {
+                configuration = this.plugin.getConfigService().loadPluginConfig(commandPlugin.getServer().getPort(), commandPlugin.getName(), "commands.yml");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            if(!configuration.configuration().contains(command.getName())) {
+                configuration.configuration().set(command.getName(), true);
+                configuration.save();
+            }
+            if(configuration.configuration().getBoolean(command.getName(), false)) {
+                command.setExecutor(executor);
+            }
+        }
+    }
+}
