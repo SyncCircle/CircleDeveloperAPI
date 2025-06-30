663853253966e125060d1a2211c30bc46d8a0ec2
Bennit B. <bennit.br+github@gmail.com>
2025-06-30 18:22:49 +0200

    fix: reload with MessageService

--- src/main/java/de/synccircle/circledeveloperapi/command/CircleCommand.java
+++ src/main/java/de/synccircle/circledeveloperapi/command/CircleCommand.java
@@ -1,15 +1,14 @@
 package de.synccircle.circledeveloperapi.command;
 
 import de.synccircle.circledeveloperapi.CircleDeveloperAPI;
-import de.synccircle.circledeveloperapi.config.Message;
-import de.synccircle.circledeveloperapi.util.Configuration;
+import de.synccircle.circledeveloperapi.config.APIMessage;
 import de.synccircle.circledeveloperapi.util.StringUtil;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.IOException;
+import java.util.Map;
 
 public class CircleCommand implements CommandExecutor {
 
@@ -28,33 +27,15 @@ public class CircleCommand implements CommandExecutor {
                 if(commandSender.hasPermission("circle.command.circle.reload")) {
                     if(strings[1].equalsIgnoreCase("messages")) {
                         if(commandSender.hasPermission("circle.command.circle.reload.messages")) {
-                            for(Message message : Message.values()) {
-                                Configuration configuration;
-                                try {
-                                    configuration = this.plugin.getConfigService().loadMessageConfig(this.plugin.getName());
-                                } catch (IOException e) {
-                                    throw new RuntimeException(e);
-                                }
-                                if(configuration.configuration().getString(message.name().toLowerCase()) == null) {
-                                    configuration.configuration().set(message.name().toLowerCase(), message.getDefaultMessage());
-                                    try {
-                                        configuration.configuration().save(configuration.file());
-                                    } catch (IOException e) {
-                                        throw new RuntimeException(e);
-                                    }
-                                    message.setMessage(StringUtil.colorizeString(message.getDefaultMessage()));
-                                    continue;
-                                }
-                                message.setMessage(StringUtil.colorizeString(configuration.configuration().getString(message.name().toLowerCase())));
-                            }
-                            commandSender.sendMessage(Message.getMessageWithPrefix(Message.COMMAND_CIRCLE_RELOAD_CONFIRM));
+                            this.plugin.getMessageService().load();
+                            commandSender.sendMessage(StringUtil.replacePlaceholder(APIMessage.getMessageWithPrefix(APIMessage.COMMAND_CIRCLE_RELOAD_CONFIRM), Map.of("%config%", "Messages")));
                             return true;
                         }
                     }
                 }
             }
         }
-        commandSender.sendMessage("ne");
+        commandSender.sendMessage(APIMessage.getMessageWithPrefix(APIMessage.COMMAND_CIRCLE_HELP));
         return true;
     }
 }
