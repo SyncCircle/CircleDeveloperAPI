f1cbde2bb6bf6206477c550c9173ead1aaddcd8a
Bennit B. <bennit.br+github@gmail.com>
2025-06-30 18:22:17 +0200

    rename: from Message to APIMessage

--- src/main/java/de/synccircle/circledeveloperapi/config/APIMessage.java
+++ src/main/java/de/synccircle/circledeveloperapi/config/APIMessage.java
@@ -4,12 +4,14 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Getter
-public enum Message {
+public enum APIMessage {
 
     PREFIX("&a&lCircle §8» "),
     NO_PERMISSION("&cDazu hast du keine Rechte!"),
     ERROR("&4ERROR"),
     LINE("&8&m---------------------------------------"),
+
+    COMMAND_CIRCLE_HELP("&7Nutze: /circle reload messages"),
     COMMAND_CIRCLE_RELOAD_CONFIRM("&7Die Konfiguration für &a%config% &7wurde neu gelauden.");
 
     private final String defaultMessage;
@@ -17,11 +19,11 @@ public enum Message {
     @Setter
     private String message;
 
-    Message(String defaultMessage) {
+    APIMessage(String defaultMessage) {
         this.defaultMessage = defaultMessage;
     }
 
-    public static String getMessageWithPrefix(Message message) {
+    public static String getMessageWithPrefix(APIMessage message) {
         return PREFIX.getMessage() + message.getMessage();
     }
 
