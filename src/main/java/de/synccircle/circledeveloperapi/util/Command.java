c7d1e16a448cc0c64aa4f2e5dea6e2d7f2ed8992
Bennit B. <bennit.br+github@gmail.com>
2025-07-05 18:27:54 +0200

    add: permission variable

--- src/main/java/de/synccircle/circledeveloperapi/util/Command.java
+++ src/main/java/de/synccircle/circledeveloperapi/util/Command.java
@@ -3,4 +3,4 @@ package de.synccircle.circledeveloperapi.util;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.plugin.Plugin;
 
-public record Command(Plugin plugin, String name, CommandExecutor executor) {}
+public record Command(Plugin plugin, String name, CommandExecutor executor, String permission) {}
