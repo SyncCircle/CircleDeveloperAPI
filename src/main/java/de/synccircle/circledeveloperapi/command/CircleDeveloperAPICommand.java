e5dd99b81e43eee8f23c38777f3c85dbc6ca7b65
Bennit B. <bennit.br+github@gmail.com>
2025-07-05 14:53:39 +0200

    add: reload for commands

--- src/main/java/de/synccircle/circledeveloperapi/command/CircleDeveloperAPICommand.java
+++ src/main/java/de/synccircle/circledeveloperapi/command/CircleDeveloperAPICommand.java
@@ -25,17 +25,26 @@ public class CircleDeveloperAPICommand implements CommandExecutor {
         if(strings.length == 2) {
             if(strings[0].equalsIgnoreCase("reload")) {
                 if(commandSender.hasPermission("circle.command.circledeveloperapi.reload")) {
+                    //Reload messages
                     if(strings[1].equalsIgnoreCase("messages")) {
                         if(commandSender.hasPermission("circle.command.circledeveloperapi.reload.messages")) {
                             this.plugin.getMessageService().load();
-                            commandSender.sendMessage(StringUtil.replacePlaceholder(MainMessage.getMessageWithPrefix(MainMessage.COMMAND_CIRCLE_RELOAD_CONFIRM), Map.of("%config%", "Messages")));
+                            commandSender.sendMessage(StringUtil.replacePlaceholder(MainMessage.getMessageWithPrefix(MainMessage.COMMAND_CIRCLEDEVELOPERAPI_RELOAD_CONFIRM), Map.of("%config%", "Messages")));
+                            return true;
+                        }
+                    }
+                    //Reload Commands
+                    if(strings[1].equalsIgnoreCase("commands")) {
+                        if(commandSender.hasPermission("circle.command.circledeveloperapi.reload.commands")) {
+                            this.plugin.getCommandService().reloadCommands();
+                            commandSender.sendMessage(StringUtil.replacePlaceholder(MainMessage.getMessageWithPrefix(MainMessage.COMMAND_CIRCLEDEVELOPERAPI_RELOAD_CONFIRM), Map.of("%config%", "Commands")));
                             return true;
                         }
                     }
                 }
             }
         }
-        commandSender.sendMessage(MainMessage.getMessageWithPrefix(MainMessage.COMMAND_CIRCLE_HELP));
+        commandSender.sendMessage(MainMessage.getMessageWithPrefix(MainMessage.COMMAND_CIRCLEDEVELOPERAPI_HELP));
         return true;
     }
 }
